45e46f3c286c3a28741bb701407f963c44e24158
Yoan <yoandiny14@gmail.com>
2026-03-31 00:21:49 +0300

    initial commit

---
+++ src/main/java/mg/yoan/igagain/IgAgainApplication.java
@@ -0,0 +1,13 @@
+package mg.yoan.igagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/yoan/igagain/IgAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.yoan.igagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
